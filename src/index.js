0c8454253e07f660ffacfc62b90986e0aae6415c
RenarsS <61384726+RenarsS@users.noreply.github.com>
2021-09-30 08:40:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const App = () => {
            <Carousel>
                 <img src={"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"} className={"slideItem"}/>
                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"} className={"slideItem"}/>
-               <iframe width="560" height="315" src="https://www.youtube.com/embed/HX9TbuuWI0c"
-    title="YouTube video player" frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen/>
            </Carousel>
     )
 
