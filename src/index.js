072bca1968af40e842be5f90a4051aa6851a17cb
RenarsS <61384726+RenarsS@users.noreply.github.com>
2021-09-30 10:36:11 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Carousel from './components/Carousel';
 const App = () => {
     return(
            <Carousel>
-                <img src={"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"} className={"slideItem"}/>
-               <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"} className={"slideItem"}/>
+               <img src={"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"} className={"slideItem"} alt={"Baby Yoda"}/>
+               <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"} className={"slideItem"} alt={"Phone"}/>
            </Carousel>
     )
 
